8f80c3214ec04be3b53ccaee47e0375cc133185c
Ahmed Bourazza <ahmedbourazza1@gmail.com>
2025-04-07 23:30:08 +0200

    first commit

---
+++ src/test/java/org/system/recipesproject/RecipesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.system.recipesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
