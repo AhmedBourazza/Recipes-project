19ea44284c5b145c0e02f6d3af0037191146718e
Ahmed Bourazza <ahmedbourazza1@gmail.com>
2025-04-11 13:51:39 +0200

    ajout de model user + configuration

---
+++ src/main/java/org/system/recipesproject/model/User.java
@@ -0,0 +1,12 @@
+package org.system.recipesproject.model;
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+
+    // Getters / Setters
+}
